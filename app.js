4658669b94a4006a3b571bee7b1225806fa9fd84
elUishi <60514571+elUishi@users.noreply.github.com>
2020-03-04 02:15:25 -0500

    juntos

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ console.log("eres privado?");
 console.log("en serio?");
 console.log("nahh?");
 console.log("1");
-<<<<<<< HEAD
+
 console.log("2");
 console.log("3");
-=======
+
 console.log("0");
->>>>>>> c8ab0d9f4f984295df869672f1745efcb7cee147
+
